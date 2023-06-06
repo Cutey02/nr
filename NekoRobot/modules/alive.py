2c9287519e841bc14f4d0e96d4ed1cb8528fc5f0
Cutey02 <116083642+Cutey02@users.noreply.github.com>
2023-06-06 17:24:12 +0530

    Update alive.py

--- NekoRobot/modules/alive.py
+++ NekoRobot/modules/alive.py
@@ -38,11 +38,11 @@ from NekoRobot.events import register
 
 edit_time = 5
 """ =======================Neko====================== """
-file1 = "https://telegra.ph/file/cd7aad1ea310312886358.png"
-file2 = "https://telegra.ph/file/48a97320463caa61dba3d.png"
-file3 = "https://telegra.ph/file/2295a7207495eccbbe298.png"
-file4 = "https://telegra.ph/file/67e0bf231a97cd2e364ea.png"
-file5 = "https://telegra.ph/file/990684ecd3d119fa9fec6.png"
+file1 = "https://graph.org/file/8b464c9cb731a0fe73bcb.jpg"
+file2 = "https://graph.org/file/8b464c9cb731a0fe73bcb.jpg"
+file3 = "https://graph.org/file/8b464c9cb731a0fe73bcb.jpg"
+file4 = "https://graph.org/file/8b464c9cb731a0fe73bcb.jpg"
+file5 = "https://graph.org/file/8b464c9cb731a0fe73bcb.jpg"
 """ =======================Neko====================== """
 
 START_TIME = datetime.utcnow()
