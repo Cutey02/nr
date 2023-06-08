32deb4b00d6fe6c8198e99a603356724ab253711
Cutey02 <116083642+Cutey02@users.noreply.github.com>
2023-06-08 19:21:27 +0530

    Update truth_and_dare.py

--- NekoRobot/modules/truth_and_dare.py
+++ NekoRobot/modules/truth_and_dare.py
@@ -1,1 +1,18 @@
+@register(pattern="^/truth ?(.*)")
 
+def truth(update: Update, context: CallbackContext):
+    args = context.args
+    update.effective_message.reply_text(random.choice(truth_and_dare_string.TRUTH))
+
+@register(pattern="^/dare ?(.*)")
+
+def dare(update: Update, context: CallbackContext):
+    args = context.args
+    update.effective_message.reply_text(random.choice(truth_and_dare_string.DARE))
+
+
+TRUTH_HANDLER = DisableAbleCommandHandler("truth", truth, run_async=True)
+DARE_HANDLER = DisableAbleCommandHandler("dare", dare, run_async=True)
+
+dispatcher.add_handler(TRUTH_HANDLER)
+dispatcher.add_handler(DARE_HANDLER)
