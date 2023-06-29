c853cd4f4ee035f6996eb30725f346bce3232140
Cutey02 <116083642+Cutey02@users.noreply.github.com>
2023-06-29 07:44:43 +0530

    Update telegraph.py

--- NekoRobot/modules/telegraph.py
+++ NekoRobot/modules/telegraph.py
@@ -73,7 +73,7 @@ async def telegrap(event):
                         [
                             types.KeyboardButtonUrl(
                                 "➡ View Telegraph",
-                                f"https://te.legra.ph/{}",
+                                f"                                f"https://te.legra.ph{media_urls[0]}",
                             )
                         ]
                     ],
@@ -109,7 +109,7 @@ async def telegrap(event):
                     [
                         types.KeyboardButtonUrl(
                             "➡ View Telegraph",
-                            f"https://te.legra.ph/{}",
+                            f"                                f"https://te.legra.ph{media_urls[0]}",
                         )
                     ]
                 ],
